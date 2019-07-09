7e9af10223f468dce3eef44697ca6e1b877296a5
Joel Hernandez <joel24hernan@gmail.com>
2019-07-08 22:53:30 -0500

    Created functioning app.

---
+++ app/data/friends.js
@@ -0,0 +1,57 @@
+// ===============================================================================
+// DATA
+// ===============================================================================
+
+var userData = [
+    {
+      "name": "Timmy",
+      "photo": "https://via.placeholder.com/150",
+      "scores":[
+          5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+      ]
+    },
+    {
+      "name": "Ruth",
+      "photo": "https://via.placeholder.com/150",
+      "scores":[
+          3,
+          2,
+          2,
+          4,
+          1,
+          1,
+          5,
+          1,
+          3,
+          1
+      ]
+    },
+    {
+      "name": "Jerry",
+      "photo": "https://via.placeholder.com/150",
+      "scores":[
+          1,
+          1,
+          1,
+          1,
+          1,
+          1,
+          1,
+          1,
+          1,
+          1
+      ]
+    }
+  ];
+  
+  // Export the array to make it accessible to other files using require.
+  module.exports = userData;
